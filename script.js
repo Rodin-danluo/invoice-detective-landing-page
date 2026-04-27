b3e69539b79b3b067ffebec89ea7a3999e45b125
Rodin-danluo <danluo020@gmail.com>
2026-04-27 17:33:32 +0800

    site: add script

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.faq-list details').forEach((item) => {
+  item.addEventListener('toggle', () => {
+    if (!item.open) {
+      return;
+    }
+
+    document.querySelectorAll('.faq-list details').forEach((other) => {
+      if (other !== item) {
+        other.open = false;
+      }
+    });
+  });
+});
